7b93634e4e2538bdb2b894c32d47d549b850e0e0
Zhou Yihang <23302010026@m.fudan.edu.cn>
2026-07-02 14:35:47 +0800

    init: 完成因果模型基本代码框架

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+from .gru import GRUClassifier
+from .tcn import TCNClassifier
+from .transformer import TransformerClassifier
+
+__all__ = [
+    "GRUClassifier",
+    "TCNClassifier",
+    "TransformerClassifier",
+]
---
+++ run.sh
@@ -0,0 +1,23 @@
+python main.py \
+    --mode full \
+    --model gru \
+    --epochs 10 \
+    --window 64 \
+    --verbose \
+    --auto_save \
+
+python main.py \
+    --mode full \
+    --model tcn \
+    --epochs 10 \
+    --window 64 \
+    --verbose \
+    --auto_save \
+
+python main.py \
+    --mode full \
+    --model transformer \
+    --epochs 10 \
+    --window 64 \
+    --verbose \
+    --auto_save \
